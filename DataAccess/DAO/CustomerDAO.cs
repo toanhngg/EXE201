d5d766a5a1b8ba32f80696349cda8070c2cc845f
Toanh <anhng130@gmail.com>
2024-01-25 17:22:21 +0700

    commit test

---
+++ DataAccess/DAO/CustomerDAO.cs
@@ -0,0 +1,42 @@
+﻿using BusinesObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAO
+{
+    internal class CustomerDAO
+    {
+        private static CustomerDAO instance = null;
+        public static readonly object instanceLock = new object();
+        private static ShiawaseContext dbcontext = new ShiawaseContext();
+        public static CustomerDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                        instance = new CustomerDAO();
+                }
+                dbcontext = new ShiawaseContext();
+                return instance;
+            }
+        }
+        public IEnumerable<Customer> listCustomer()
+        {
+            var listCus = new List<Customer>();
+            try
+            {
+                listCus = dbcontext.Customers.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return listCus;
+        }
+    }
+}
--- DataAccess/Responsitory/ICustomerResponsitory.cs
+++ DataAccess/Responsitory/ICustomerResponsitory.cs
@@ -1,5 +1,4 @@
 ﻿using BusinesObject.Models;
-using DataAccess.DAO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +7,8 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Responsitory
 {
-    internal class CustomerResponsitory : ICustomerResponsitory
+    public interface ICustomerResponsitory
     {
-        public IEnumerable<Customer> GetCustomer() => CustomerDAO.Instance.listCustomer();
-
+        IEnumerable<Customer> GetCustomer();
     }
 }
